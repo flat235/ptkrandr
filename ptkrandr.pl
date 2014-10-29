6d9dd09ae7819ccfc970d1dec493c7d91ea4e2c1
Moritz Bellach <github@moritzbellach.de>
2014-10-29 16:35:25 +0100

    cleanup. added current resolution

--- ptkrandr.pl
+++ ptkrandr.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 
 use warnings;
-#use strict;
+use strict;
 
 use Tk;
 
@@ -10,7 +10,6 @@ my $script = $0;
 my @lines = `xrandr`;
 my $screen = -1;
 my @screens;
-my $state = 0;
 my $res = 0;
 my $monitor = 0;
 
@@ -21,11 +20,12 @@ for my $line (@lines){
 		$screen = $1;
 	}
 	elsif($line =~ /^\s*(\d+x\d+\S*)/){
-		$screens[$screen]->{$monitor}->[$res] = $1;
+		$screens[$screen]->{$monitor}->{'resolutions'}->[$res] = $1;
 		$res++;
 	}
-	elsif ($line =~ /^(\S+)\s+connected/){
+	elsif ($line =~ /^(\S+)\s+connected\s+(\d+x\d+)/){
 		$monitor = $1;
+		$screens[$screen]->{$monitor}->{'currentresolution'} = $2;
 		$res = 0;
 	}
 	else{
@@ -33,23 +33,21 @@ for my $line (@lines){
 		$res = 0;
 	}
 }
-
-$mw = MainWindow->new(-title => 'Monitor Konfiguration');
 $screen = 0;
+my $mw = MainWindow->new(-title => 'Monitor Configuration');
 for my $curscreen (@screens) {
 	my $frame = $mw->Frame;
 	$frame->Label(-text => 'Screen ' . "$screen")->pack;
-	for my $curmon (keys($screens[$screen])){
+	$screen++;
+	for my $curmon (keys($curscreen)){
 		my $subframe = $frame->Frame;
-		$subframe->Label(-text => $curmon)->pack;
-		$subframe->Button(-text => 'Automatisch erkennen', -command => sub { system('xrandr --output ' . $curmon . ' --auto'); exec($script);})->pack(-side => 'top');
-		for my $curres (@{$screens[$screen]->{$curmon}}){
+		$subframe->Label(-text => $curmon . ': ' . $curscreen->{$curmon}->{'currentresolution'})->pack;
+		$subframe->Button(-text => 'Auto dectect', -command => sub { system('xrandr --output ' . $curmon . ' --auto'); exec($script);})->pack(-side => 'top');
+		for my $curres (@{$curscreen->{$curmon}->{'resolutions'}}){
 			$subframe->Button(-text => "$curres", -command => sub { system('xrandr --output ' . $curmon . ' --mode ' . $curres); exec $script;})->pack(-side => 'top');
 		}
 		$subframe->pack(-side => 'left');
 	}
 	$frame->pack(-side => 'left');
-	$screen++;
 }
-$mw->Button(-text => 'Beenden', -command => sub { exit; })->pack(-side => 'bottom');
 MainLoop;
